81fdaa5b0f38e27f5372c79fdbd267d342fcd57e
Scott Cameron <scott@iohouse.com>
2023-06-02 18:51:00 +0800

    Added chapter 10 Unity project files.

---
+++ ch10/Unity Project/Assets/Scripts/ConstantDamage.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(HealthSystem))]
+public class ConstantDamage : MonoBehaviour, IDamage
+{
+    public LayerMask DamageMask => _damageMask;
+    [SerializeField] private LayerMask _damageMask;
+
+    public int DamageAmount => _damageAmount;
+    [SerializeField] private int _damageAmount;
+
+    [SerializeField] private float _damageInterval = 5f;
+
+    private void Start() => StartCoroutine(ApplyDamageOverTime());
+
+    private IEnumerator ApplyDamageOverTime()
+    {
+        var healthSystem = GetComponent<HealthSystem>();
+        while (true)
+        {
+            healthSystem.HandleDamageCollision(null, this);
+            yield return new WaitForSeconds(_damageInterval);
+        }
+    }
+
+    public void DoDamage(Collider collision, bool isAffected)
+    {
+        // Do something when the damage is applied.
+    }
+}
