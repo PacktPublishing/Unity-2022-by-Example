fc5be98b19ea7068cfd68ae6e1c772f146738d67
Scott Cameron <scott@iohouse.com>
2023-06-28 19:35:48 +0800

    Added chapter 11 Unity project files.

---
+++ ch11/Unity Project/Assets/Scripts/TriggeredEvent.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+//[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Collider))]
+public class TriggeredEvent : MonoBehaviour
+{
+    [Tooltip("Requires the player character to have the 'Player' tag assigned.")]
+    public bool IsTriggeredByPlayer = true;
+
+    public UnityEvent OnTriggered;
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (IsTriggeredByPlayer
+            && !collision.CompareTag(Tags.Player))
+            return;
+
+        OnTriggered?.Invoke();
+    }
+}
